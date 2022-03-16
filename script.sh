b9277215530c1b4197e421de626350e67e23aafa
nerfZael <bogunovij@gmail.com>
2022-03-16 19:19:19 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+mkdir /home/ubuntu/polywrap
+git clone https://github.com/polywrap/nodes.git /home/ubuntu/polywrap
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+. ~/.profile
+cd /home/ubuntu/polywrap/nodes/persistence-node
+nvm install && nvm use
+
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
+source ~/.profile
+
 npm install yarn -g
 npm install pm2 -g
 pm2 startup
 yarn
 yarn build
-pm2 start build/main.js api --http 80
