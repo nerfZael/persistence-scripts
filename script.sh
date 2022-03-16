5151975dc22b449879b3e30ae0630651fe2be5cc
nerfZael <bogunovij@gmail.com>
2022-03-16 20:10:29 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 mkdir /home/ubuntu/polywrap
 git clone https://github.com/polywrap/nodes.git /home/ubuntu/polywrap
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
-. ~/.profile
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+
 cd /home/ubuntu/polywrap/nodes/persistence-node
 nvm install && nvm use
 
