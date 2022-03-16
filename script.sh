36787821453f776d82524fff4b108a91360f6a30
nerfZael <bogunovij@gmail.com>
2022-03-16 19:12:55 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-mkdir /home/ubuntu/polywrap
-git clone https://github.com/polywrap/nodes.git /home/ubuntu/polywrap
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.profile
-cd /home/ubuntu/polywrap/nodes/persistence-node
 nvm install && nvm use
 npm install yarn -g
 npm install pm2 -g
