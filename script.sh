b369260f6398d1488b22c75cde3624753243806d
nerfZael <bogunovij@gmail.com>
2022-03-17 16:43:53 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,10 +16,12 @@ source ~/.profile
 npm install yarn -g
 npm install pm2 -g
 
-env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.13.0/bin /home/ubuntu/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
 yarn
 yarn build
 
 pm2 start bin/main.js -- api --http 8080
+
+pm2 startup
+env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.13.0/bin /home/ubuntu/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
 pm2 save
