ff63be1882d3dbcc733e05d7597de7eeb193a33d
nerfZael <bogunovij@gmail.com>
2022-03-23 21:24:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,7 +29,7 @@ yarn build
 curl -o- https://raw.githubusercontent.com/nerfZael/persistence-scripts/main/.env > .env
 
 pm2 start ipfs -- daemon
-pm2 start bin/main.js -- past -b 100000 --log
+node bin/main.js -- past -b 100000 --log
 pm2 start bin/main.js -- daemon --http 8081 --log
 
 pm2 startup
