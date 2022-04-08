2666b1d9bb650af9eaf72e1dfa3ffa74a2c4c422
nerfZael <bogunovij@gmail.com>
2022-04-09 01:19:07 +0200

    Update nerfZael.sh

--- nerfZael.sh
+++ nerfZael.sh
@@ -27,16 +27,3 @@ source ~/.profile
 
 npm install yarn -g
 npm install pm2 -g
-
-yarn
-yarn build
-curl -o- https://raw.githubusercontent.com/nerfZael/persistence-scripts/main/.env > .env
-
-pm2 start ipfs --max-memory-restart 900M -- daemon
-node bin/main.js past -b 500000 --log
-pm2 start bin/main.js -- daemon --http 8081 --log
-
-pm2 startup
-sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v16.13.0/bin /home/ubuntu/.npm-global/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
-
-pm2 save
